a342c608210aa83b20cd11a73a389ab34232a45e
dan <dan@192.168.86.9>
2017-06-27 23:41:49 -0300

    first commit

---
+++ AESCryptography/src/com/danimv/security/cbc/CBCEncrypt.java
@@ -0,0 +1,92 @@
+/**
+ * @author danimv 
+ * 
+ */
+
+package com.danimv.security.cbc;
+
+import java.security.MessageDigest;
+import java.security.SecureRandom;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+public class CBCEncrypt {
+	final static Logger logger = Logger.getLogger(CBCEncrypt.class.toString());
+
+    public static void main(String[] args) throws Exception {
+    	BasicConfigurator.configure();
+        String key = "gugudadagugudada";
+        String clean = "You can't always get what you want, But if you try sometimes well you might find, You get what you need";
+        
+        byte[] encrypted = encrypt(clean, key);
+        logger.debug("To be Encrypted: " + clean);
+        logger.debug("Encrypted" + encrypted);
+        String decrypted = decrypt(encrypted, key);
+        logger.debug("Result : " + decrypted );
+    }
+
+    public static byte[] encrypt(String plainText, String key) throws Exception {
+        byte[] clean = plainText.getBytes();
+
+        // Generating IV.
+        int ivSize = 16;
+        byte[] iv = new byte[ivSize];
+        SecureRandom random = new SecureRandom();
+        random.nextBytes(iv);
+        IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+
+        // Hashing key.
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        digest.update(key.getBytes("UTF-8"));
+        byte[] keyBytes = new byte[16];
+        System.arraycopy(digest.digest(), 0, keyBytes, 0, keyBytes.length);
+        SecretKeySpec secretKeySpec = new SecretKeySpec(keyBytes, "AES");
+
+        // Encrypt.
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivParameterSpec);
+        byte[] encrypted = cipher.doFinal(clean);
+
+        // Combine IV and encrypted part.
+        byte[] encryptedIVAndText = new byte[ivSize + encrypted.length];
+        System.arraycopy(iv, 0, encryptedIVAndText, 0, ivSize);
+        System.arraycopy(encrypted, 0, encryptedIVAndText, ivSize, encrypted.length);
+
+        return encryptedIVAndText;
+    }
+
+    public static String decrypt(byte[] encryptedIvTextBytes, String key) throws Exception {
+        int ivSize = 16;
+        int keySize = 16;
+
+        // Extract IV.
+        byte[] iv = new byte[ivSize];
+        System.arraycopy(encryptedIvTextBytes, 0, iv, 0, iv.length);
+        IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+
+        // Extract encrypted part.
+        int encryptedSize = encryptedIvTextBytes.length - ivSize;
+        byte[] encryptedBytes = new byte[encryptedSize];
+        System.arraycopy(encryptedIvTextBytes, ivSize, encryptedBytes, 0, encryptedSize);
+
+        // Hash key.
+        byte[] keyBytes = new byte[keySize];
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        md.update(key.getBytes());
+        System.arraycopy(md.digest(), 0, keyBytes, 0, keyBytes.length);
+        SecretKeySpec secretKeySpec = new SecretKeySpec(keyBytes, "AES");
+
+        // Decrypt.
+        Cipher cipherDecrypt = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipherDecrypt.init(Cipher.DECRYPT_MODE, secretKeySpec, ivParameterSpec);
+        byte[] decrypted = cipherDecrypt.doFinal(encryptedBytes);
+
+        return new String(decrypted);
+    }
+}
